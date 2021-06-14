4564fe234f993c71d1118eefed967079e1f07c21
nikolayshuklin <nikolayshuklin@mail.ru>
2021-06-14 21:30:28 +0300

    Добавил item_note

--- app/src/main/java/ru/geekbrains/notes/ui/list/NoteListFragment.java
+++ app/src/main/java/ru/geekbrains/notes/ui/list/NoteListFragment.java
@@ -5,17 +5,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+import java.util.zip.Inflater;
+
 import ru.geekbrains.notes.R;
+import ru.geekbrains.notes.domain.Note;
+import ru.geekbrains.notes.domain.NoteRepository;
+import ru.geekbrains.notes.domain.NoteRepositoryImpl;
 
 public class NoteListFragment extends Fragment {
+
+    private NoteRepository noteRepository;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        noteRepository = new NoteRepositoryImpl();
     }
 
     @Nullable
@@ -28,6 +39,18 @@ public class NoteListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         LinearLayout noteList = view.findViewById(R.id.note_list_container);
+        List<Note> notes = noteRepository.getNotes();
+        for (Note note: notes){
+            View itemView = LayoutInflater.from(requireContext()).inflate(R.layout.item_note, noteList, false);
+            TextView name = itemView.findViewById(R.id.note_name);
+            TextView description = itemView.findViewById(R.id.description);
+            TextView date = itemView.findViewById(R.id.date);
+            name.setText(note.getName());
+            description.setText(note.getDescription());
+            date.setText(note.getDate());
+            noteList.addView(itemView);
+
+        }
 
     }
 
